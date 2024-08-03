a9fb3490eb5e12f36249c154ea957dac9314bdde
Maria Del Mar <mariadelmar.artunduaga911@gmail.com>
2024-08-02 23:25:38 -0500

    se hizo la clase empleado

---
+++ POO/Ejercicios/Entity/Jefe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios.Entity
+{
+    internal class Jefe: Empleado
+    {
+        public string Area { get; set; }
+        public Jefe(int id, string nombreCompleto, int edad, string puesto, int salario, string area)
+           : base(id, nombreCompleto, edad, puesto, salario)
+        {
+            Area = area;
+        }
+    }
+}
---
+++ POO/Ejercicios/Program.cs
@@ -0,0 +1,21 @@
+﻿using Ejercicios.Entity;
+using Ejercicios.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEmpleado empleado = new Empleado(1, "Maria Del Mar", 18, "Desarrolladora de Software", 3000);
+
+            // Mostrar la información del empleado
+            empleado.MostrarInformacion();
+        }
+    }
+}
