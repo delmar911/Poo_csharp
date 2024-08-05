a5035db5b8d56ff006a7bc3c1cfeec461a0c85d2
delmar911 <mariadelmar.artunduaga911@gmail.com>
2024-08-05 10:04:33 -0500

    Reestructuracion del proyecto

---
+++ POO/Ejercicios/Interface/IRecibirFlete.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicios.Interface
+{
+    public interface IRecibirFlete
+    {
+        void RecibirFlete(int precio)
+    }
+}
